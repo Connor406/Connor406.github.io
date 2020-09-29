32a8a6f3dd76585b930a0c247257fb345e62208b
Connor M <63883267+Connor406@users.noreply.github.com>
2020-09-28 20:27:47 -0400

    Updates

---
+++ precache-manifest.cfd1f70d78c1004302638d7f13f56674.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297f6e59054c9257985e70f4d1f6f7c5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4dab9cf4cc57b515512e",
+    "url": "/my-app/static/css/main.03c30055.chunk.css"
+  },
+  {
+    "revision": "2b657b9c4690791a9478",
+    "url": "/my-app/static/js/2.0757ab1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.0757ab1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dab9cf4cc57b515512e",
+    "url": "/my-app/static/js/main.5548deac.chunk.js"
+  },
+  {
+    "revision": "4f282b9659b4936d5d46",
+    "url": "/my-app/static/js/runtime-main.922ac3ad.js"
+  },
+  {
+    "revision": "e868e85a4747fea9fbdd27ad9cb8e698",
+    "url": "/my-app/static/media/Resume.e868e85a.pdf"
+  },
+  {
+    "revision": "aeb60701c7135e2d01e5ab7ce0fd28f6",
+    "url": "/my-app/static/media/TradesmithRegular.aeb60701.otf"
+  },
+  {
+    "revision": "fdd10873f9b2b8ffe414ef939a36cf23",
+    "url": "/my-app/static/media/TuesdayNight-Regular.fdd10873.otf"
+  },
+  {
+    "revision": "ec67fc8b080bb536b9a794db8f5789bb",
+    "url": "/my-app/static/media/pexels-life-of-pix-8892.ec67fc8b.jpg"
+  }
+]);
